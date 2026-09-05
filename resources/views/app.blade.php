5a2faace30e7b2eabff6d03c43a1d09c2896045b
Bruno Kay <kaybruno177@gmail.com>
2026-09-05 12:48:47 -0300

    feat(prospeccao): implementar lead automatico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="/images/logo-kaytech.png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
