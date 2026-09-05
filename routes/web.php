9e0cf36d3530062e580ac72e4d083a8d86987776
Bruno Kay <kaybruno177@gmail.com>
2026-09-05 13:54:39 -0300

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,11 @@ use App\Http\Controllers\Admin\TestimonialAdminController;
 use App\Http\Controllers\Admin\FinanceController;
 use App\Http\Controllers\Admin\ClientController;
 
-// Subdomínio brunokay.kaytech.com.br → LINKTREE (bio-link) do Bruno.
-// O portfólio completo fica em kaytech.com.br/brunokay (path).
+// Subdomínio brunokay.kaytech.com.br → PORTFÓLIO completo do Bruno.
+// O linktree (bio-link) fica em kaytech.com.br/brunokay (path).
 Route::domain('brunokay.kaytech.com.br')->group(function () {
-    Route::get('/', [LinkPageController::class, 'brunokayLinks'])->name('brunokay.home');
+    Route::get('/', [LinkPageController::class, 'brunokay'])->name('brunokay.home');
+    Route::get('/links', [LinkPageController::class, 'brunokayLinks'])->name('brunokay.domain.links');
     Route::post('/contato', [BrunoContactController::class, 'store'])->name('brunokay.domain.contact.store');
 });
 
